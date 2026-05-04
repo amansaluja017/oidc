704e2a0774227e1596e00e7198ec38f282f6d8df
aman <amansaluja017@gmail.com>
2026-05-05 04:39:16 +0530

    fix origin issue

--- src/module/dto/client.dto.ts
+++ src/module/dto/client.dto.ts
@@ -5,8 +5,8 @@ import BaseDto from "../../common/utils/dto.utils.ts";
 class RegisterDto extends BaseDto {
     static schema: z.ZodObject = z.object({
         name: z.string().nonempty().min(2).max(255),
-        domain: z.url().min(2).max(255).nonempty(),
-        redirectUrl: z.url().nonempty(),
+        domain: z.url().min(2).max(255).nonempty().transform(val => val.replace(/\/+$/, "")),
+        redirectUrl: z.url().nonempty().transform(val => val.replace(/\/+$/, "")),
     }).strict()
 };
 
--- src/module/server.ts
+++ src/module/server.ts
@@ -13,7 +13,6 @@ export default function startServer() {
 
         app.use(cors({origin: async function(origin, cb) {
             if (!origin) return cb(null, true);
-            console.log(origin, "origin");
 
             if (origin === "http://localhost:3000" || origin === "http://localhost:3001" || origin === "https://oidc-t4w5.onrender.com" || origin === "https://one-million-checkboxes-qf98.onrender.com") {
                 return cb(null, true);
